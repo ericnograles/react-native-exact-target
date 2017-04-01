76d58ef928272097a33daf300d512d53281e8867
Eric Nograles <grales@gmail.com>
2017-04-01 14:48:02 -0400

    adds a dummy method to initialize push, adds JB4A-SDK artifacts

--- ios/RNExactTarget.m
+++ ios/RNExactTarget.m
@@ -7,7 +7,13 @@
 {
     return dispatch_get_main_queue();
 }
-RCT_EXPORT_MODULE()
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(initializePushManager:(NSString *)etAppId location:(NSString *)etAccessToken)
+{
+    RCTLogInfo(@"Pretending to initialize ETPush with app ID %@ and access token %@", etAppId, etAccessToken);
+}
 
 @end
   
