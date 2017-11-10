2b373ae109233407c16402a5f47088cdc378692e
Jinwoong Hwang <jin.hwang@zocdoc.com>
2017-11-10 16:04:16 -0500

    from PR #8

--- ios/RNExactTarget.h
+++ ios/RNExactTarget.h
@@ -11,7 +11,23 @@
 #import <React/RCTEventEmitter.h>
 #endif
 
+/**
+ Please note that this is a singleton object, and you should reference it as [RNExactTarget pushManager].
+ */
+
 @interface RNExactTarget : RCTEventEmitter <RCTBridgeModule>
+/**
+ Returns (or initializes) the shared pushManager instance.
+ @return The singleton instance of an RNExactTarget pushManager.
+ */
++(nullable instancetype)pushManager;
+-(instancetype _Nonnull)init;
+
+- (void)didRegisterUserNotificationSettings:(UIUserNotificationSettings *_Nonnull)notificationSettings;
+- (void)didRegisterForRemoteNotificationsWithDeviceToken:(NSData *_Nonnull)deviceToken;
+- (void)didFailToRegisterForRemoteNotificationsWithError:(NSError *_Nonnull)error;
+- (void)handleRemoteNotification:(NSDictionary *_Nullable)userInfo;
+- (void)handleLocalNotification:(UILocalNotification *_Nullable)localNotification;
 
 @end
   
